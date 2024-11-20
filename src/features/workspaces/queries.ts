c2eca4c1b44f9b0a10d67b2237f6156759dab29b
PrinceKex <ehigiator.ukeke@gmail.com>
2024-11-20 22:56:21 +0100

    workspace join page and query added

--- src/features/workspaces/queries.ts
+++ src/features/workspaces/queries.ts
@@ -56,3 +56,29 @@ export const getWorkspace = async ({ workspaceId }: GetWorkspaceProps) => {
   return null
  }
 }
+
+interface GetWorkspaceInfoProps {
+ workspaceId: string
+}
+
+export const getWorkspaceInfo = async ({
+ workspaceId,
+}: GetWorkspaceInfoProps) => {
+ try {
+  const { databases } = await createSessionClient()
+  // const user = await account.get()
+
+  // const member = await getMember({ databases, workspaceId, userId: user.$id })
+  // if (!member) return null
+
+  const workspace = await databases.getDocument<Workspace>(
+   DATABASE_ID,
+   WORKSPACES_ID,
+   workspaceId
+  )
+
+  return { name: workspace.name }
+ } catch {
+  return null
+ }
+}
