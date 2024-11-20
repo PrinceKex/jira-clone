d9aed2154c223982aa1a19b4e6a51d76b8aaa5d3
PrinceKex <ehigiator.ukeke@gmail.com>
2024-11-20 23:40:55 +0100

    join workspace function done

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -2,11 +2,11 @@ import { getCurrent } from '@/features/auth/queries'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
-const WorkspaceIdPage = async () => {
+const WorkspaceIdPage = async ({ params }) => {
  const user = await getCurrent()
  if (!user) redirect('/sign-in')
 
- return <div>WorkspaceIdPage</div>
+ return <div>WorkspaceId: {params.workspaceId}</div>
 }
 
 export default WorkspaceIdPage
