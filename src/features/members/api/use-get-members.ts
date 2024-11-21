4a7ccc096084612933fe64067f07c24c0b8bdae7
PrinceKex <ehigiator.ukeke@gmail.com>
2024-11-21 23:19:45 +0100

    use get member api added

---
+++ src/features/members/api/use-get-members.ts
@@ -0,0 +1,22 @@
+import { useQuery } from '@tanstack/react-query'
+import { client } from '@/lib/rpc'
+
+interface UseGetMembersProps {
+ workspaceId: string
+}
+
+export const useGeMembers = ({ workspaceId }: UseGetMembersProps) => {
+ const query = useQuery({
+  queryKey: ['members', workspaceId],
+  queryFn: async () => {
+   const response = await client.api.members.$get({ query: { workspaceId } })
+   if (!response.ok) {
+    throw new Error('Failed to fetch workspaces')
+   }
+
+   const { data } = await response.json()
+   return data
+  },
+ })
+ return query
+}
