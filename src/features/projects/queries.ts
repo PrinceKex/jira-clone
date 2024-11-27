f20d2da89875ae35aed2da24c45f84b25fc48fbb
PrinceKex <ehigiator.ukeke@gmail.com>
2024-11-27 22:44:08 +0100

    loading and error pages added for all routes

--- src/features/projects/queries.ts
+++ src/features/projects/queries.ts
@@ -39,27 +39,25 @@ interface GetProjectProps {
 }
 
 export const getProject = async ({ projectId }: GetProjectProps) => {
- try {
-  const { databases, account } = await createSessionClient()
-  const user = await account.get()
+ const { databases, account } = await createSessionClient()
+ const user = await account.get()
 
-  const project = await databases.getDocument<Project>(
-   DATABASE_ID,
-   PROJECTS_ID,
-   projectId
-  )
+ const project = await databases.getDocument<Project>(
+  DATABASE_ID,
+  PROJECTS_ID,
+  projectId
+ )
 
-  const member = await getMember({
-   databases,
-   workspaceId: project.workspaceId,
-   userId: user.$id,
-  })
-  if (!member) return null
-
-  return project
- } catch {
-  return null
+ const member = await getMember({
+  databases,
+  workspaceId: project.workspaceId,
+  userId: user.$id,
+ })
+ if (!member) {
+  throw new Error('Unauthorized')
  }
+
+ return project
 }
 
 // interface GetWorkspaceInfoProps {
