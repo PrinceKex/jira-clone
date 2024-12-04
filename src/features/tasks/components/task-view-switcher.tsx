58650fbeb4533892992f8bbc4aa6c8ce73f5e3a1
PrinceKex <ehigiator.ukeke@gmail.com>
2024-12-04 22:55:48 +0100

    data table function done

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -11,6 +11,8 @@ import { UseWorkspaceId } from '@/features/workspaces/hooks/use-workspace-id'
 import { useQueryState } from 'nuqs'
 import { DataFilters } from './data-filters'
 import { useTaskFilters } from '../hooks/use-task-filters'
+import { DataTable } from './data-table'
+import { columns } from './columns'
 
 export const TaskViewSwitcher = () => {
  const [{ status, assigneeId, projectId, dueDate }] = useTaskFilters()
@@ -62,7 +64,7 @@ export const TaskViewSwitcher = () => {
     ) : (
      <>
       <TabsContent value='table' className='mt-0'>
-       {JSON.stringify(tasks)}
+       <DataTable columns={columns} data={tasks?.documents ?? []} />
       </TabsContent>
       <TabsContent value='kanban' className='mt-0'>
        {JSON.stringify(tasks)}
