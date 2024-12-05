59d06d25f974a680fead58ee1a7525eb523a28a6
PrinceKex <ehigiator.ukeke@gmail.com>
2024-12-05 15:13:20 +0100

    use delete and update task functions added

--- src/features/tasks/api/use-update-task.ts
+++ src/features/tasks/api/use-update-task.ts
@@ -5,29 +5,27 @@ import { useRouter } from 'next/navigation'
 import { toast } from 'sonner'
 
 type ResponseType = InferResponseType<
- (typeof client.api.tasks)[':taskId']['$delete'],
+ (typeof client.api.tasks)[':taskId']['$patch'],
  200
 >
 type RequestType = InferRequestType<
- (typeof client.api.tasks)[':taskId']['$delete']
+ (typeof client.api.tasks)[':taskId']['$patch']
 >
 
-export const useDeleteTask = () => {
+export const useUpdateTask = () => {
  const router = useRouter()
  const queryClient = new QueryClient()
  const mutation = useMutation<ResponseType, Error, RequestType>({
-  mutationFn: async ({ param }) => {
-   const response = await client.api.tasks[':taskId']['$delete']({
-    param,
-   })
+  mutationFn: async ({ param, json }) => {
+   const response = await client.api.tasks[':taskId']['$patch']({ param, json })
    if (!response.ok) {
-    throw new Error('Failed to delete Task')
+    throw new Error('Failed to update Task')
    }
    return await response.json()
   },
   onSuccess: ({ data }) => {
-   toast.success('Task deleted successfully')
-   //  router.refresh()
+   toast.success('Task updated successfully')
+   router.refresh()
    queryClient.invalidateQueries({ queryKey: ['tasks'] })
    queryClient.invalidateQueries({ queryKey: ['tasks', data.$id] })
   },
--- src/features/tasks/components/task-actions.tsx
+++ src/features/tasks/components/task-actions.tsx
@@ -2,12 +2,16 @@ import {
  DropdownMenuContent,
  DropdownMenuItem,
 } from '@/components/ui/dropdown-menu'
+import { useConfirm } from '@/hooks/use-confirm'
 import {
  DropdownMenu,
  DropdownMenuTrigger,
 } from '@radix-ui/react-dropdown-menu'
 import { ExternalLinkIcon, PencilIcon, TrashIcon } from 'lucide-react'
 import React from 'react'
+import { useDeleteTask } from '../api/use-delete-task'
+import { useRouter } from 'next/navigation'
+import { UseWorkspaceId } from '@/features/workspaces/hooks/use-workspace-id'
 
 interface TaskActionsProps {
  id: string
@@ -16,36 +20,59 @@ interface TaskActionsProps {
 }
 
 export const TaskActions = ({ id, projectId, children }: TaskActionsProps) => {
+ const workspaceId = UseWorkspaceId()
+ const router = useRouter()
+ const [ConfirmDialog, confirm] = useConfirm(
+  'Delete task',
+  'This action cannot be undone',
+  'destructive'
+ )
+ const { mutate, isPending } = useDeleteTask()
+
+ const onDelete = async () => {
+  const ok = await confirm()
+  if (!ok) return
+
+  mutate({ param: { taskId: id } })
+ }
+
+ const onOpenTask = () => {
+  router.push(`/workspaces/${workspaceId}/tasks/${id}`)
+ }
+ const onOpenProject = () => {
+  router.push(`/workspaces/${workspaceId}/projects/${projectId}`)
+ }
  return (
   <div className='flex justify-end'>
+   <ConfirmDialog />
    <DropdownMenu modal={false}>
     <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
     <DropdownMenuContent align='end' className='w-48'>
      <DropdownMenuItem
       onClick={() => {}}
-      disabled={false}
+      //disabled={false}
       className='font-medium p-[10px]'
      >
       <ExternalLinkIcon className='size-4 mr-2 stroke-2' /> Task Details
      </DropdownMenuItem>
      <DropdownMenuItem
-      onClick={() => {}}
-      disabled={false}
+      onClick={onOpenTask}
+      //disabled={false}
       className='font-medium p-[10px]'
      >
       <ExternalLinkIcon className='size-4 mr-2 stroke-2' /> Open Project
      </DropdownMenuItem>
      <DropdownMenuItem
-      onClick={() => {}}
-      disabled={false}
+      onClick={onOpenProject}
+      //disabled={false}
       className='font-medium p-[10px]'
      >
       <PencilIcon className='size-4 mr-2 stroke-2' /> Edit Task
      </DropdownMenuItem>
 
      <DropdownMenuItem
-      onClick={() => {}}
-      disabled={false}
+      onClick={onDelete}
+      disabled={isPending}
       className='text-amber-700 focus:text-amber-700 font-medium p-[10px]'
      >
       <TrashIcon className='size-4 mr-2 stroke-2' /> Delete Task
