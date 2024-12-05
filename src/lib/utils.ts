d0cf9e41936bc4e0322e07e34f0d612b374467f0
PrinceKex <ehigiator.ukeke@gmail.com>
2024-12-05 14:14:36 +0100

    task action and columns function  for data table added

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -14,3 +14,16 @@ export function generateInviteCode(length: number) {
  }
  return result
 }
+
+// export function snakeToTitleCase(str: string) {
+//  return str
+//   .split('_')
+//   .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+//   .join(' ')
+// }
+export function snakeToTitleCase(str: string) {
+ return str
+  .toLowerCase()
+  .replace(/_/g, ' ')
+  .replace(/\b\w/g, (char) => char.toUpperCase())
+}
