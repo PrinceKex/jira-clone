f4a3607d8d583beb356548f954d78bf3510bc4f9
PrinceKex <ehigiator.ukeke@gmail.com>
2024-12-09 22:32:49 +0100

    kanban card function done

--- src/features/tasks/components/data-kanban.tsx
+++ src/features/tasks/components/data-kanban.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import { Task, TaskStatus } from '../types'
-import { DragDropContext } from 'hello-pangea/dnd'
+import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd'
 import { KanbanColumnHeader } from './kanban-column-header'
+import { KanbanCard } from './kanban-card'
 
 const boards: TaskStatus[] = [
  TaskStatus.BACKLOG,
@@ -49,6 +50,29 @@ export const DataKanban = ({ data }: DataKanbanProps) => {
       <h2 className='text-sm font-semibold text-muted-foreground'>
        <KanbanColumnHeader board={board} taskCount={tasks[board].length} />
       </h2>
+      <Droppable droppableId={board}>
+       {(provided) => (
+        <div
+         {...provided.droppableProps}
+         ref={provided.innerRef}
+         className='min-h-[200px] py-1.5'
+        >
+         {tasks[board].map((task, index) => (
+          <Draggable key={task.$id} draggableId={task.$id} index={index}>
+           {(provided) => (
+            <div
+             {...provided.draggableProps}
+             {...provided.dragHandleProps}
+             ref={provided.innerRef}
+            >
+             <KanbanCard task={task} />
+            </div>
+           )}
+          </Draggable>
+         ))}
+        </div>
+       )}
+      </Droppable>
      </div>
     ))}
    </div>
