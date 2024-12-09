66b2d0d7dd56f636e4cfcb59d509c079188e58ec
PrinceKex <ehigiator.ukeke@gmail.com>
2024-12-10 00:30:49 +0100

    data kanban component added

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button'
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { TabsContent } from '@radix-ui/react-tabs'
 import { Loader, PlusIcon } from 'lucide-react'
-import React from 'react'
+import React, { useCallback } from 'react'
 import { useCreateTaskModal } from '../hooks/use-create-task-modal'
 import { useGetTasks } from '../api/use-get-tasks'
 import { UseWorkspaceId } from '@/features/workspaces/hooks/use-workspace-id'
@@ -14,6 +14,7 @@ import { useTaskFilters } from '../hooks/use-task-filters'
 import { DataTable } from './data-table'
 import { columns } from './columns'
 import { DataKanban } from './data-kanban'
+import { TaskStatus } from '../types'
 
 export const TaskViewSwitcher = () => {
  const [{ status, assigneeId, projectId, dueDate }] = useTaskFilters()
@@ -31,6 +32,13 @@ export const TaskViewSwitcher = () => {
   status,
   dueDate,
  })
+
+ const onKanbanChange = useCallback(
+  (tasks: { $id: string; status: TaskStatus; position: number }[]) => {
+   console.log(tasks)
+  },
+  []
+ )
  return (
   <Tabs
    defaultValue={view}
@@ -69,7 +77,7 @@ export const TaskViewSwitcher = () => {
       </TabsContent>
       <TabsContent value='kanban' className='mt-0'>
        {JSON.stringify(tasks)}
-       <DataKanban data={tasks?.documents ?? []} />
+       <DataKanban data={tasks?.documents ?? []} onChange={onKanbanChange} />
       </TabsContent>
       <TabsContent value='calender' className='mt-0'>
        {JSON.stringify(tasks)}
