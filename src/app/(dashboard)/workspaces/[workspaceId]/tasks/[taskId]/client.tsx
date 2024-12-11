78d5229b40d128603a20dfe7687cfe27fe218896
PrinceKex <ehigiator.ukeke@gmail.com>
2024-12-11 12:29:29 +0100

    task breadcrumbs task description and task overview function done

--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -1,6 +1,12 @@
 'use client'
+import { DottedSeparator } from '@/components/dotted-separator'
+import { PageError } from '@/components/page-error'
+import { PageLoader } from '@/components/page-loader'
 import { useGetTask } from '@/features/tasks/api/use-get-task'
 import { useTaskId } from '@/features/tasks/api/use-task-id'
+import { TaskBreadcrumbs } from '@/features/tasks/components/task-breadcrumbs'
+import { TaskDescription } from '@/features/tasks/components/task-description'
+import { TaskOverview } from '@/features/tasks/components/task-overview'
 
 import React from 'react'
 
@@ -10,9 +16,17 @@ export const TaskIdClient = () => {
  if (isLoading) {
   return <PageLoader />
  }
+ if (!data) {
+  return <PageError message='Task not found' />
+ }
  return (
-  <div>
-   <p>Task Id Client</p>
+  <div className='flex flex-col'>
+   <TaskBreadcrumbs project={data.project} task={data} />
+   <DottedSeparator className='my-6' />
+   <div className='grid grid-cols-1 lg:grid-cols-2'>
+    <TaskOverview task={data} />
+    <TaskDescription task={data} />
+   </div>
   </div>
  )
 }
